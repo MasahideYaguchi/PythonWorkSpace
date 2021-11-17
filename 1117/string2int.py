e81fdbade9564a2a86057504772063f38055154d
Masahide Yaguchi <aqym3580@ezweb.ne.jp>
2021-11-17 11:52:01 +0900

    更新

--- 1117/string2int.py
+++ 1117/string2int.py
@@ -1,6 +1,12 @@
 def str2int(s):
  """str2int 文字列を数値に変換した値を返す"""
- return 'a' == int(s)
+ if type(s) is str:
+     if s.isnumeric():#数値かどうか確認
+       return int(s)#true
+     else:
+       return 0
+ else:
+  return s #文字列ではなかった時
 print(str2int('a'))
 print(str2int('10'))
 print(str2int(100))
